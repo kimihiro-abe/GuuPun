d97cb7e4e8d11e5be36ee83e77b7a0debf319e07
kimihiro-abe <kimihiro.abe@gmail.com>
2025-05-05 23:36:36 +0900

    更新:3rdApp

--- GuuPun/src/main/java/com/example/demo/app/chitchat_cue/controller/ChitChat_Controller.java
+++ GuuPun/src/main/java/com/example/demo/app/chitchat_cue/controller/ChitChat_Controller.java
@@ -1,7 +1,7 @@
 package com.example.demo.app.chitchat_cue.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.app.chitchat_cue.entity.Chatcue;
 import com.example.demo.app.chitchat_cue.service.ChatcueService;
+import com.example.demo.app.common.ConstCue;
 
 @Controller
 @RequestMapping("/chitchat_cue")
-public class ChitChat_Controller {
+public class ChitChat_Controller implements ConstCue {
 	
     private final ChatcueService chatcueService;
 
@@ -24,15 +25,31 @@ public class ChitChat_Controller {
     // ルートURL（/）にアクセスしたときにサイトマップを表示
     @GetMapping()
     public String showSitemap(Model model) {
+    	String message_header = ConstRead();
+    	System.out.println("message_header:" + message_header);
+    	model.addAttribute("message_header", message_header);
         return "chitchat_cue/index";  // templates/index.html を返す
     }
     
     // Buttonの押下後の処理
     @GetMapping("/showCue")
     public String showCue(Model model) {
+    	String message_header = ConstRead();
+    	System.out.println("message_header:" + message_header);
+    	
     	Chatcue chatcue = chatcueService.findOne();
     	model.addAttribute("chatcue", chatcue);
+    	model.addAttribute("message_header", message_header);
         return "chitchat_cue/index";
     }
+    
+    public String ConstRead() {
+    	Random random = new Random();
+    	List<String> headerList = ConstCue.CUE_HEADER;
+    	String selected = headerList.get(random.nextInt(headerList.size()));
+    	return selected;
+    }
+    
+    
 
 }
