9c586a870b460b69ad3abcc7705587c3d85575c5
kimihiro-abe <kimihiro.abe@gmail.com>
2025-05-16 02:47:45 +0900

    DB管理：環境変数まわり記述の変更

--- GuuPun/src/main/java/com/example/demo/GuuPunApplication.java
+++ GuuPun/src/main/java/com/example/demo/GuuPunApplication.java
@@ -3,12 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableScheduling
 @SpringBootApplication
 public class GuuPunApplication {
 
 	public static void main(String[] args) {
+		// OSの環境変数読み込みチェック
+		System.out.println(System.getenv("DB_URL"));
+			
+		// app.start
 		SpringApplication.run(GuuPunApplication.class, args);
 		
 		// 揮発型ToDo List : Forgotten Mimolette
